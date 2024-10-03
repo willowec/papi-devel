6eaece38e3ba0290da539e9e0b8b1a4960b05699
Willow Cunningham <willow.e.cunningham@maine.edu>
2024-10-03 16:49:11 -0400

    back to when delete happened

--- src/validation_tests/load_store_testcode.c
+++ src/validation_tests/load_store_testcode.c
@@ -6,18 +6,19 @@ int execute_stores(int n) {
 #if defined(__aarch64__)
 
 	__asm(  ".data\n"
-            "stvar: .word 1 /* stvar in memory */\n"
-            ".text\n"
-            "   ldr x2, =stvar\n"
-            "   mov x1, #0\n"
-            "str_loop:"
-		    "   str x1, [x2]\n"
-            "   add x1, x1, #1\n"
-            "   cmp x1, [n]\n"
-            "   bne str_loop\n"
-		    :
-		    : [n] "r" (n)
-		    : "cc" /* clobbered */
+		"stvar: .word 1 /* stvar in memory */\n"
+		".text\n"
+		"	ldr x2, =stvar /* address of stvar */\n"
+		"	mov x4, %0\n"
+		"	mov x1, #0\n"
+		"str_loop:\n"
+		"	str x1, [x2] /* store into stvar */\n"
+		"	add x1, x1, #1\n"
+		"	cmp x1, x4\n"
+		"	bne str_loop\n"
+		:
+		: "r" (n)
+		: "cc" /* clobbered */
 	);
 
 	return 0;
@@ -33,18 +34,19 @@ int execute_loads(int n) {
 #if defined(__aarch64__)
 
 	__asm(  ".data\n"
-            "ldvar: .word 1 /* ldvar in memory */\n"
-            ".text\n"
-            "   ldr x2, =ldvar\n"
-            "   mov x1, #0\n"
-            "ldr_loop:"
-		    "   ldr x3, [x2]\n"
-            "   add x1, x1, x3\n"
-            "   cmp x1, [n]\n"
-            "   bne ldr_loop\n"
-		    :
-		    : [n] "r" (n)
-		    : "cc" /* clobbered */
+		"ldvar: .word 1 /* ldvar in memory */\n"
+		".text\n"
+		"	ldr x2, =ldvar /* address of ldvar */\n"
+		"	mov x4, %0\n"
+		"	mov x1, #0\n"
+		"ldr_loop:\n"
+		"	ldr x3, [x2] /* load from ldvar */\n"
+		"	add x1, x1, x3\n"
+		"	cmp x1, x4\n"
+		"	bne ldr_loop\n"
+		:
+		: "r" (n)
+		: "cc" /* clobbered */
 	);
 
 	return 0;
